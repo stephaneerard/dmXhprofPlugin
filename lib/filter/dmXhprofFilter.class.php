5d7a377b31325e70d020cafbcb272e6aa9e9218a
Stéphane Erard <stephane.erard@gmail.com>
2011-08-31 23:24:23 +0200

    refactoring code

--- lib/filter/dmXhprofFilter.class.php
+++ lib/filter/dmXhprofFilter.class.php
@@ -5,10 +5,10 @@ class dmXhprofFilter extends dmInitFilter
 	{
 		if(extension_loaded('xhprof') && sfConfig::get('app_xhprof_enabled'))
 		{
-			$pluginPath = sfConfig::get('sf_plugins_dir') . '/dmXhprofPlugin/lib/vendor/xhprof/'; 
+			$pluginPath = sfConfig::get('sf_plugins_dir') . '/dmXhprofPlugin/lib/vendor/xhprof/';
 			require_once $pluginPath . 'xhprof_lib/utils/xhprof_lib.php';
 			require_once $pluginPath . 'xhprof_lib/utils/xhprof_runs.php';
-				
+
 			if($this->getContext()->getRequest()->isXmlHttpRequest())
 			{
 				$this->getContext()->getEventDispatcher()->connect('dm.context.end', array($this, 'listenToDmContextEndForXhr'));
@@ -22,40 +22,44 @@ class dmXhprofFilter extends dmInitFilter
 
 		$filterChain->execute($filterChain);
 	}
-	
+
 	protected function getXHProfUILink($profiler_namespace, $run_id)
 	{
-		return sprintf('http://%s/index.php?run=%s&source=%s', 
-			sfConfig::get('app_xhprof_ui.link', 'localhost/xhprof/xhprof_html/'), 
-			$run_id, 
-			$profiler_namespace);
+		return sprintf('http://%s/index.php?run=%s&source=%s',
+		sfConfig::get('app_xhprof_ui.link', 'localhost/xhprof/xhprof_html/'),
+		$run_id,
+		$profiler_namespace);
+	}
+
+	protected function getNamespace()
+	{
+		return dmArray::last(explode(DIRECTORY_SEPARATOR, sfConfig::get('sf_root_dir'))) . '_' . sfConfig::get('sf_environment');
+	}
+
+	protected function saveRun()
+	{
+		$profiler_namespace = $this->getNamespace();
+		$xhprof_data = xhprof_disable();
+		$xhprof_runs = new XHProfRuns_Default();
+		$run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace);
+		return $this->getXHProfUILink($profiler_namespace, $run_id);
 	}
 
 	public function listenToDmContextEndForXhr($event)
 	{
-		$profiler_namespace = dmArray::last(explode(DIRECTORY_SEPARATOR, sfConfig::get('sf_root_dir')));
-		$xhprof_data = xhprof_disable();
-		$xhprof_runs = new XHProfRuns_Default();
-		$run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace);
-
-		$profiler_url = $this->getXHProfUILink($profiler_namespace, $run_id);
+		$profiler_url = $this->saveRun();
 		$this->getContext()->getEventDispatcher()->notify(new sfEvent($this, 'application.log', array($profiler_url)));
 	}
 
 	public function listenToDmContextEndForHtml($event)
 	{
-		$profiler_namespace = dmArray::last(explode(DIRECTORY_SEPARATOR, sfConfig::get('sf_root_dir')));
-		$xhprof_data = xhprof_disable();
-		$xhprof_runs = new XHProfRuns_Default();
-		$run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace);
+		$profiler_url = $this->saveRun();
 
-		$profiler_url = $this->getXHProfUILink($profiler_namespace, $run_id);
-		
 		if(sfConfig::get('app_xhprof_outputlink', false))
 		{
 			echo '<a href="'.$profiler_url.'" target="_blank">Profiler output</a>';
 		}
-		
+
 		$this->getContext()->getEventDispatcher()->notify(new sfEvent($this, 'application.log', array($profiler_url)));
 	}
 }
